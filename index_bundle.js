961b7ca086f18b5f0e7b9739e9732fe14f22ddcb
Taylor Milliman <twm013@bucknell.edu>
2016-06-29 11:51:18 -0400

    version 1.0

--- index_bundle.js
+++ index_bundle.js
@@ -4607,7 +4607,7 @@
 	  { history: browserHistory },
 	  React.createElement(
 	    Route,
-	    { path: '/recipe-box', component: MainContainer },
+	    { path: './', component: MainContainer },
 	    React.createElement(IndexRoute, { component: Main }),
 	    React.createElement(Route, { path: 'recipeView', component: RecipeViewContainer })
 	  )
